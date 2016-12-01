a24c8218640c5ce44e18fe405ef3405f399831b3
Darlene Zouras <dzouras@gmail.com>
2016-12-01 12:06:00 -0600

    AddGame php file updated, working.

--- FinalProjectHTML/addgame.php
+++ FinalProjectHTML/addgame.php
@@ -2,24 +2,24 @@
 //Turn on error reporting
 ini_set('display_errors', 'On');
 //Connects to the database
-//$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "moyerjo-db", "EpoJM8FxtVi7AW2d", "moyerjo-db");
-$mysqli = new mysqli("localhost", "root", "root", "nintendoDB");
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "moyerjo-db", "EpoJM8FxtVi7AW2d", "moyerjo-db");
+// $mysqli = new mysqli("localhost", "root", "root", "nintendoDB");
 
 if(!$mysqli || $mysqli->connect_errno){
 	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
 	}
 
 if($_POST['homeland'] >= 0){
-	if(!($stmt = $mysqli->prepare("INSERT INTO characters(name, raceOrSpecies, homeland) VALUES (?,?,?)"))){
+	if(!($stmt = $mysqli->prepare("INSERT INTO games(gameTitle, releaseDay, releaseMonth, releaseYear, setting, releaseSystem) VALUES (?,?,?,?,?,?)"))){
 		echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
 	}
 }
-else{
+/* else{
 	if(!($stmt = $mysqli->prepare("INSERT INTO characters(name, raceOrSpecies, homeland) VALUES (?,?,NULL)"))){
 		echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
 	}
-}
-if(!($stmt->bind_param("ssi",$_POST['cName'],$_POST['raceOrSpecies'],$_POST['homeland']))){
+}　*/
+if(!($stmt->bind_param("siiiii",$_POST['gName'],$_POST['gDay'],$_POST['gMonth'],$_POST['gYear'],$_POST['gSetting'],$_POST['gSystem']))){
 	echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
 }
 if(!$stmt->execute()){
